678d194d51fbb36f6c3d712069e13c1f9a38fdf8
ptishkin <104498557+ptishkin@users.noreply.github.com>
2025-04-20 04:05:49 +0300

    Update outputs.tf

--- kube/outputs.tf
+++ kube/outputs.tf
@@ -1,11 +1,13 @@
 output "external_v4_endpoint" {
   value       = module.kube.external_v4_endpoint
   description = "The kube api endpoint"
+  sensitive   = true
 }
 
 output "cluster_ca_certificate" {
   value       = module.kube.cluster_ca_certificate
   description = "The kube ca cert"
+  sensitive   = true
 }
 
 output "cluster_id" {
