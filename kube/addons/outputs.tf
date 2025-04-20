de2fc4392e522e3e592794767fa3a8bd6ea30837
ptishkin <104498557+ptishkin@users.noreply.github.com>
2025-04-20 16:41:58 +0300

    Update outputs.tf

--- kube/addons/outputs.tf
+++ kube/addons/outputs.tf
@@ -1,5 +1,5 @@
 output "service_loadbalancer_ip" {
-  value       = module.addons.ingress_nginx.service_loadbalancer_ip
+  value       = module.addons.ingress_nginx_status
   description = "The LB IP of the kuber"
   sensitive   = true
 }
